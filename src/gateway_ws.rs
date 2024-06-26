8f8dc6cbcbdbe9979f5c5be9884d029201cae9bd
niooii <wangh3@bxscience.edu>
2024-06-26 01:20:24 -0400

    aaa

--- src/gateway_ws.rs
+++ src/gateway_ws.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use eetf::Term;
+use num_derive::FromPrimitive;
 use serde::de::DeserializeOwned;
 use serde::Deserialize;
 use tokio_tungstenite::tungstenite::Message;
@@ -25,8 +26,414 @@ use std::sync::Mutex;
 use serde_eetf::{to_bytes, from_bytes};
 use anyhow::Result;
 
-pub enum GatewayOpCode {
+pub enum GatewayError {
+    Custom { text: String }
+}
 
+// https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway-gateway-close-event-codes
+#[derive(Debug, FromPrimitive, PartialEq, Eq, Copy, Clone)]
+#[repr(u32)]
+pub enum GatewayOpCode {
+    /*
+    TYPE: Receive
+    An event was dispatched.
+    */
+    Dispatch = 0,
+    
+    /*
+    TYPE: Send/Receive
+    Fired periodically by the client to keep the connection alive.
+    */
+    Heartbeat = 1,
+    
+    /*
+    TYPE: Send
+    Starts a new session during the initial handshake.
+    */
+    Identify = 2,
+    
+    /*
+    TYPE: Send
+    Update the client's presence.
+    */
+    PresenceUpdate = 3,
+    
+    /*
+    TYPE: Send
+    Used to join/leave or move between voice channels.
+    */
+    VoiceStateUpdate = 4,
+    
+    /*
+    TYPE: Send
+    Resume a previous session that was disconnected.
+    */
+    Resume = 6,
+    
+    /*
+    TYPE: Receive
+    You should attempt to reconnect and resume immediately.
+    */
+    Reconnect = 7,
+    
+    /*
+    TYPE: Send
+    Request information about offline guild members in a large guild.
+    */
+    RequestGuildMembers = 8,
+    
+    /*
+    TYPE: Receive
+    The session has been invalidated. You should reconnect and identify/resume accordingly.
+    */
+    InvalidSession = 9,
+    
+    /*
+    TYPE: Receive
+    Sent immediately after connecting, contains the heartbeat_interval to use.
+    */
+    Hello = 10,
+    
+    /*
+    TYPE: Receive
+    Sent in response to receiving a heartbeat to acknowledge that it has been received.
+    */
+    HeartbeatAck = 11,
+}
+#[derive(Serialize, PartialEq, Debug)]
+struct GatewaySendEventRaw
+{
+    op: u32,
+    d: serde_json::Value,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+struct GatewayReceiveEventRaw
+{
+    t: Option<String>,
+    op: u32,
+    s: u32,
+    d: serde_json::Value,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+// #[serde(rename_all(deserialize = "PascalCase"))]
+// TODO! me in tommorow please move everything related to dispatch events into its own file its too much for this one thanks
+pub enum GatewayDispatchEvent {
+
+}
+
+impl GatewayDispatchEvent {
+    fn from_raw(data_raw: GatewayReceiveEventRaw) -> Result<Self, GatewayError> {
+        let dispatch_type = data_raw.t.unwrap();   
+        // TODO!  
+        // https://stackoverflow.com/questions/45059538/how-to-deserialize-into-a-enum-variant-based-on-a-key-name
+        // #[serde(flatten)]
+        match dispatch_type.as_str() {
+            // Defines the heartbeat interval
+            "HELLO" => {
+                todo!()
+            },
+            // Contains the initial state information
+            "READY" => {
+                todo!()
+            },
+            // Response to Resume
+            "RESUMED" => {
+                todo!()
+            },
+            // Server is going away, client should reconnect to gateway and resume
+            "RECONNECT" => {
+                todo!()
+            },
+            // Failure response to Identify or Resume or invalid active session
+            "INVALID_SESSION" => {
+                todo!()
+            },
+            // Application command permission was updated
+            "APPLICATION_COMMAND_PERMISSIONS_UPDATE" => {
+                todo!()
+            },
+            // Auto Moderation rule was created
+            "AUTO_MODERATION_RULE_CREATE" => {
+                todo!()
+            },
+            // Auto Moderation rule was updated
+            "AUTO_MODERATION_RULE_UPDATE" => {
+                todo!()
+            },
+            // Auto Moderation rule was deleted
+            "AUTO_MODERATION_RULE_DELETE" => {
+                todo!()
+            },
+            // Auto Moderation rule was triggered and an action was executed
+            "AUTO_MODERATION_ACTION_EXECUTION" => {
+                todo!()
+            },
+            // New guild channel created
+            "CHANNEL_CREATE" => {
+                todo!()
+            },
+            // Channel was updated
+            "CHANNEL_UPDATE" => {
+                todo!()
+            },
+            // Channel was deleted
+            "CHANNEL_DELETE" => {
+                todo!()
+            },
+            // Message was pinned or unpinned
+            "CHANNEL_PINS_UPDATE" => {
+                todo!()
+            },
+            // Thread created, also sent when being added to a private thread
+            "THREAD_CREATE" => {
+                todo!()
+            },
+            // Thread was updated
+            "THREAD_UPDATE" => {
+                todo!()
+            },
+            // Thread was deleted
+            "THREAD_DELETE" => {
+                todo!()
+            },
+            // Sent when gaining access to a channel, contains all active threads in that channel
+            "THREAD_LIST_SYNC" => {
+                todo!()
+            },
+            // Thread member for the current user was updated
+            "THREAD_MEMBER_UPDATE" => {
+                todo!()
+            },
+            // Some user(s) were added to or removed from a thread
+            "THREAD_MEMBERS_UPDATE" => {
+                todo!()
+            },
+            // Entitlement was created
+            "ENTITLEMENT_CREATE" => {
+                todo!()
+            },
+            // Entitlement was updated or renewed
+            "ENTITLEMENT_UPDATE" => {
+                todo!()
+            },
+            // Entitlement was deleted
+            "ENTITLEMENT_DELETE" => {
+                todo!()
+            },
+            // Lazy-load for unavailable guild, guild became available, or user joined a new guild
+            "GUILD_CREATE" => {
+                todo!()
+            },
+            // Guild was updated
+            "GUILD_UPDATE" => {
+                todo!()
+            },
+            // Guild became unavailable, or user left/was removed from a guild
+            "GUILD_DELETE" => {
+                todo!()
+            },
+            // A guild audit log entry was created
+            "GUILD_AUDIT_LOG_ENTRY_CREATE" => {
+                todo!()
+            },
+            // User was banned from a guild
+            "GUILD_BAN_ADD" => {
+                todo!()
+            },
+            // User was unbanned from a guild
+            "GUILD_BAN_REMOVE" => {
+                todo!()
+            },
+            // Guild emojis were updated
+            "GUILD_EMOJIS_UPDATE" => {
+                todo!()
+            },
+            // Guild stickers were updated
+            "GUILD_STICKERS_UPDATE" => {
+                todo!()
+            },
+            // Guild integration was updated
+            "GUILD_INTEGRATIONS_UPDATE" => {
+                todo!()
+            },
+            // New user joined a guild
+            "GUILD_MEMBER_ADD" => {
+                todo!()
+            },
+            // User was removed from a guild
+            "GUILD_MEMBER_REMOVE" => {
+                todo!()
+            },
+            // Guild member was updated
+            "GUILD_MEMBER_UPDATE" => {
+                todo!()
+            },
+            // Response to Request Guild Members
+            "GUILD_MEMBERS_CHUNK" => {
+                todo!()
+            },
+            // Guild role was created
+            "GUILD_ROLE_CREATE" => {
+                todo!()
+            },
+            // Guild role was updated
+            "GUILD_ROLE_UPDATE" => {
+                todo!()
+            },
+            // Guild role was deleted
+            "GUILD_ROLE_DELETE" => {
+                todo!()
+            },
+            // Guild scheduled event was created
+            "GUILD_SCHEDULED_EVENT_CREATE" => {
+                todo!()
+            },
+            // Guild scheduled event was updated
+            "GUILD_SCHEDULED_EVENT_UPDATE" => {
+                todo!()
+            },
+            // Guild scheduled event was deleted
+            "GUILD_SCHEDULED_EVENT_DELETE" => {
+                todo!()
+            },
+            // User subscribed to a guild scheduled event
+            "GUILD_SCHEDULED_EVENT_USER_ADD" => {
+                todo!()
+            },
+            // User unsubscribed from a guild scheduled event
+            "GUILD_SCHEDULED_EVENT_USER_REMOVE" => {
+                todo!()
+            },
+            // Guild integration was created
+            "INTEGRATION_CREATE" => {
+                todo!()
+            },
+            // Guild integration was updated
+            "INTEGRATION_UPDATE" => {
+                todo!()
+            },
+            // Guild integration was deleted
+            "INTEGRATION_DELETE" => {
+                todo!()
+            },
+            // User used an interaction, such as an Application Command
+            "INTERACTION_CREATE" => {
+                todo!()
+            },
+            // Invite to a channel was created
+            "INVITE_CREATE" => {
+                todo!()
+            },
+            // Invite to a channel was deleted
+            "INVITE_DELETE" => {
+                todo!()
+            },
+            // Message was created
+            "MESSAGE_CREATE" => {
+                todo!()
+            },
+            // Message was edited
+            "MESSAGE_UPDATE" => {
+                todo!()
+            },
+            // Message was deleted
+            "MESSAGE_DELETE" => {
+                todo!()
+            },
+            // Multiple messages were deleted at once
+            "MESSAGE_DELETE_BULK" => {
+                todo!()
+            },
+            // User reacted to a message
+            "MESSAGE_REACTION_ADD" => {
+                todo!()
+            },
+            // User removed a reaction from a message
+            "MESSAGE_REACTION_REMOVE" => {
+                todo!()
+            },
+            // All reactions were explicitly removed from a message
+            "MESSAGE_REACTION_REMOVE_ALL" => {
+                todo!()
+            },
+            // All reactions for a given emoji were explicitly removed from a message
+            "MESSAGE_REACTION_REMOVE_EMOJI" => {
+                todo!()
+            },
+            // User was updated
+            "PRESENCE_UPDATE" => {
+                todo!()
+            },
+            // Stage instance was created
+            "STAGE_INSTANCE_CREATE" => {
+                todo!()
+            },
+            // Stage instance was updated
+            "STAGE_INSTANCE_UPDATE" => {
+                todo!()
+            },
+            // Stage instance was deleted or closed
+            "STAGE_INSTANCE_DELETE" => {
+                todo!()
+            },
+            // User started typing in a channel
+            "TYPING_START" => {
+                todo!()
+            },
+            // Properties about the user changed
+            "USER_UPDATE" => {
+                todo!()
+            },
+            // Someone joined, left, or moved a voice channel
+            "VOICE_STATE_UPDATE" => {
+                todo!()
+            },
+            // Guild's voice server was updated
+            "VOICE_SERVER_UPDATE" => {
+                todo!()
+            },
+            // Guild channel webhook was created, updated, or deleted
+            "WEBHOOKS_UPDATE" => {
+                todo!()
+            },
+            // User voted on a poll
+            "MESSAGE_POLL_VOTE_ADD" => {
+                todo!()
+            },
+            // User removed a vote on a poll
+            "MESSAGE_POLL_VOTE_REMOVE" => {
+                todo!()
+            },
+            _ => {
+                Err(
+                    GatewayError::Custom { text: format!("Unknown dispatch type: {}", dispatch_type) }
+                )
+            }
+        }
+    }
+}
+
+pub enum GatewayRecieveEvent {
+    DispatchEvent{ e: GatewayDispatchEvent }
+}
+
+impl GatewayRecieveEvent {
+    
+
+    pub fn from_raw(data_raw: GatewayReceiveEventRaw) -> Result<Self, GatewayError> {
+        match FromPrimitive::from_u32(data_raw.op).unwrap() {
+            GatewayOpCode::Dispatch => {
+                Ok(Self::into_dispatch_event(data_raw)?)
+            }
+            _ => {
+                Err(
+                    GatewayError::Custom {text: "Unhandled op code".to_string()}
+                )
+            }
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug)]
@@ -71,23 +478,7 @@ struct D {
     client_state: ClientState,
 }
 
-#[derive(Serialize, PartialEq, Debug)]
-struct GatewaySendEvent
-{
-    op: u32,
-    d: serde_json::Value,
-}
-
-#[derive(Deserialize, PartialEq, Debug)]
-struct GatewayReceiveEvent
-{
-    t: Option<String>,
-    op: u32,
-    s: u32,
-    d: serde_json::Value,
-}
-
-impl GatewaySendEvent {
+impl GatewaySendEventRaw {
     pub fn login(token: String, capabilities: u32, properties: Properties, presence: Presence, compress: bool, client_state: ClientState) -> Result<Self> {
         use serde_json::Value;
         let mut map = HashMap::<String, Value>::new();
@@ -149,7 +540,7 @@ pub async fn test(token: &str) -> Result<()> {
     };
 
     // Create a new GatewayLogin instance
-    let gateway_login = GatewaySendEvent::login(
+    let gateway_login = GatewaySendEventRaw::login(
         token.to_string(),
         30717,
         properties,
